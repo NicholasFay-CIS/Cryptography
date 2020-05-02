66c8800cac8cd233dc299bedd15e8279e1f7cbec
lvandeca <64226833+lvandeca@users.noreply.github.com>
2020-05-01 21:11:27 -0600

    Add files via upload

--- ass3.py
+++ ass3.py
@@ -11,13 +11,14 @@ def find_primes(n):
 	# iterate from 2 to n since 1 is not a prime
 	for i in range(2, n):
 		# iterate from 2 to i
-		for j in range(2, i):
-			#if the i mod j is zero it is not a prime
+		for j in list_of_primes:
+			# if the i mod j is zero it is not a prime
 			if(i % j) == 0:
 				break
 		else:
 			# otherwise it is a prime
 			list_of_primes.append(i)
+			# print(list_of_primes)
 	print("There are {} primes. {}".format(len(list_of_primes), list_of_primes))
 	return list_of_primes
 
@@ -27,6 +28,8 @@ def fast_powering_alg(x, exp, mod):
 	int, int, int -> int
 	This function is supposed to replicate the fast powering algorithm.
 	"""
+	if exp == 2:
+		return (x**2) % mod
 	my_expos = list()
 	calculated_list = list()
 	return_list = list()
@@ -106,36 +109,50 @@ def calc_gcd2(N):
 	return a
 
 
-def Miller_Rabine_test(p: int, k: int) -> str:
+def Miller_Rabin_test(n, k):
 	"""
-	:param p: prime > 3 to test
-	:param k: number tests to perform to check that p is prime
-	:return: string either detailing prime or composite
-	"""
-	# find d and r such that (2 ** r) * d = p - 1
-	d = p - 1
-	r = 1
-	while d % 2 == 0:
-		r +=1
-		d = d / 2
+    Miller-Rabin primality test.
 
-	# number of random integers (a) we will test to check is p is prime
-	for item in range(k):
-		a = random.randint(1,p -1)
-		while True:
-			x = fast_powering_alg(a, d, p)
-			if x == 1 or x == p - 1:
-				break
-			else:
-				caller = False
-				for i in range(r -1):
-					x = fast_powering_alg(x, 2, p)
-					if x == 1:
-						caller = True
-				if caller:
-					break
-			return "composite"
-	return "probably prime"
+    A return value of False means n is certainly not prime. A return value of
+    True means n is very likely a prime.
+    """
+	# find r, d such that n-1 = 2**r*d
+	r = 0
+	d = n - 1
+	while d % 2 == 0:
+		d >>= 1
+		r += 1
+	# check they're equal
+	assert (2 ** r * d == n - 1)
+
+	# function testing if a**d, a**2d, ... , a**rd is equal to 1 (mod n) or n-1(mod n)
+	def test_composite(a):
+		if pow(a, d, n) == 1:
+			return False
+		for i in range(r):
+			if pow(a, 2 ** i * d, n) == n - 1:
+				return False
+		return True
+
+	# runs through the test_composite function k times to check
+	# different bases. The greater k is the more likely n
+	# is to be prime
+	for i in range(k):  # number of trials
+		a = random.randrange(2, n)
+		is_composite = test_composite(a)
+		if is_composite:
+			return False
+
+	return True
+
+
+def generate_prime(b, k):
+	while True:
+		bottom_range, top_range = 2**(b-1), (2**b)-1
+		n = random.randrange(bottom_range + 1, top_range, 2)
+		if Miller_Rabin_test(n, k):
+			break
+	return n
 
 
 def remainder(x, m):
@@ -159,13 +176,64 @@ def remainder(x, m):
   return X % M_i
 
 
+def prime_factors(s, n):
+	while (n % 2 == 0):
+		s.add(2)
+		n = n // 2
+	# n must be odd at this po. So we can
+	# skip one element (Note i = i +2)
+	square_root = int(math.sqrt(n))
+	for i in range(3, square_root, 2):
+		while (n % i == 0):
+			s.add(i)
+			n = n // i
+		# This condition is to handle the case
+	# when n is a prime number greater than 2
+	if (n > 2):
+		s.add(n)
+
+
+def primitive_root(n):
+	s = set()
+	phi = n - 1
+	prime_factors(s, phi)
+	for r in range(2, phi + 1):
+		flag = False
+		for it in s:
+			# Check if r^((phi)/primefactors)
+			# mod n is 1 or not
+			if (pow(r, phi // it, n) == 1):
+				flag = True
+				break
+		# If there was no power with value 1.
+		if (flag == False):
+			return r
+
+
 def main():
 	# question 1
+	print("Question 1")
 	find_primes(100)
+	find_primes(367400)
+	print()
+
+
+
 	# question 2
-	#fast_powering_alg(76, 2, )
+	print("Question 2:")
+	base = 2
+	exp = 10
+	exp2 = 15
+	mod = 100000
+	fp = (10 ** 15)
+	fp2 = fast_powering_alg(base, fp, mod)
+	print("{}^({}^{}) (mod {}) = {}".format(base, exp, exp2, mod, fp2))
+	print()
+
+
 
 	# question 3
+	print("Question 3:")
 
 	# x (mod n) 197189 (mod 999979)
 	x = 197189
@@ -181,30 +249,52 @@ def main():
 	N = [887519, 146744, 388025, 880464, 189074]
 	gcd = calc_gcd2(N)
 	print("gcd( {} , {} , {} , {} ,  {}): {}".format(N[0], N[1], N[2], N[3], N[4], gcd))
+	print()
+
+
 
 	# question 4
-
-	# check that 1223 is prime
-	prime = 7177
+	print("Question 4:")
+	# check that 5915587277 is prime
+	prime = 5915587277
 	num_tests = 10
-	r = Miller_Rabine_test(prime, num_tests)
-	print("Miller-Rabine algorithm with {} tests states that {} is {}.".format(num_tests, prime, r))
-
+	r = Miller_Rabin_test(prime, num_tests)
+	print("Miller-Rabin with {} tests. Is {} prime? {}".format(num_tests, prime, r))
 	# check that 561 is not prime
 	prime = 561
 	num_tests = 10
-	r = Miller_Rabine_test(prime, num_tests)
-	print("Miller-Rabine algorithm with {} tests states that {} is {}.".format(num_tests, prime, r))
+	r = Miller_Rabin_test(prime, num_tests)
+	print("Miller-Rabin with {} tests. Is {} prime? {}".format(num_tests, prime, r))
+	# generate prime over 1000 bits (302 digits)
+	bits = 1000
+	probable_prime = generate_prime(bits, 10)
+	print("A probable prime over {} bits = {}".format(bits, probable_prime))
+	print()
+
+
 
 	# qustion 5
+	print("Question 5:")
     # wikipedia example
 	n = [3, 4, 5]
-
 	a = [2, 3, 1]
-
-
 	x = remainder(a, n)
 	print("Chinese Remainder thm:\nList X = {}\nList M = {}\nOutput: {}".format(a,n,x))
+	print()
+
+
+
+	# question 6
+	print("Question 6a:")
+	prime = 37055228012567588205472561716198899109643
+	num_tests = 10
+	r = Miller_Rabin_test(prime, num_tests)
+	print("Miller-Rabin with {} tests. Is {} prime? {}".format(num_tests, prime, r))
+	print()
+	print("Question 6b:")
+	prime = 37055228012567588205472561716198899109643
+	g = primitive_root(prime)
+	print("The smallest primitive root of {} = {}".format(prime, g))
 
 if __name__ == '__main__':
 	main()

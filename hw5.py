3db3c2b36346e1c80d4db5a28305fbd15a0674c5
Naser Alkhateri <nalkhate@uoregon.edu>
2020-05-27 03:37:22 +0400

    added prob3

--- hw5.py
+++ hw5.py
@@ -28,6 +28,66 @@ def squares(a, N):
 			return root
 		x += 1
 
+def find_B(X):
+	"""
+    L(X) = e^(√(lnX)(ln lnX))
+    B = L(N)^(1/√2)
+    """
+	L_x = (math.e ** math.sqrt(math.log(X) * math.log(math.log(X))))
+
+	L_n = L_x ** (1 / math.sqrt(2))
+
+	return L_n
+
+def F_t(T,N):
+	return T**2 - N
+
+
+def factor_list(list,p):
+	"""
+	Updates the list's values where it's divisible
+
+	"""
+
+	isDivisible = False
+	for i in range(len(list)):
+		if list[i] % p == 0:
+			isDivisible = True
+			list[i] = int(list[i] / p)
+	if isDivisible:
+		factor_list(list,p)
+
+	return
+
+
+def quad_sieve(N):
+	"""
+	F(T) = T^2 - N
+	a = [√N] + 1
+	#F(a), F(a+1), F(a+2),...,F(b)
+	still needs work....
+	"""
+
+	#prime basis from 2 to 11
+	#TODO: 2 to B?
+	primes = (2,3,5,7,11)
+	print("N =",N)
+	print("B =",primes[-1])
+	a = math.floor(math.sqrt(N)) + 1
+	#F(a), F(a+1), F(a+2),...,F(b)
+
+	F_list = []
+
+	#example
+	for a_i in range(0,a+1):
+		F_list.append(int(F_t(a + a_i,N)))
+	print(F_list)
+	for prime in primes:
+		factor_list(F_list,prime)
+	print(F_list)
+
+	#some steps
+
 def main():
 	problem2 = True
 	while(problem2 == True):
@@ -38,6 +98,9 @@ def main():
 		if(next_input == "yes" or next_input == "Yes"):
 			continue
 		problem2 = False
+
+	#find_B(221)
+	quad_sieve(221)
 	return
 
 main()
